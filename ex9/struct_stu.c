bf64b1791450fa6f301ade82cc51c1eec061ea83
gongsounghyun <sounghyun4846@gmail.com>
2020-10-20 21:23:18 +0900

    9번 문제 동적 할당 구조체 수정

--- ex9/struct_stu.c
+++ ex9/struct_stu.c
@@ -1,44 +1,45 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-struct grade{
-    char name[20];    
-    int middle_grade;
-    int final_grade;
+#include <search.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct node {
+	char *name;
+	int middle;
+	int final;
 };
+struct node *root = NULL;
 
+int compare(const void *cp1, const void *cp2){
+	return strcmp(((struct node *)cp1)->name, ((struct node *)cp2)->name);
+}
+void print_node(const void *nodeptr, VISIT order, int level){
+	if (order == preorder || order == leaf)
+		printf("이름 = %-20s, 중간 성적 = %d, 기말 성적 = %d\n", (*(struct node **)nodeptr)->name, (*(struct node **)nodeptr)->middle, (*(struct node **)nodeptr)->final);
+	}
 int main(){
-    int middle, final, count, i;
-    char name;
-    char search_name[20];
-    printf("학생의 수를 입력해주십시오.");
-    scanf("%d", &count);
-
-    struct grade *gra = (struct grade*)malloc(sizeof(struct grade)*count);
-    for(int i=0;i<count;i++){
-        printf("학생의 이름과 중간 성적 기말 성적을 띄어쓰기로 구분후 입력.");
-	scanf("%s %d %d",gra[i].name, &gra[i].middle_grade, &gra[i].final_grade);
-    }
-    for(int i=0;i<(sizeof(struct grade)/28)*count;i++){
-	printf("name : %s, middle_grade : %d, final_grade : %d\n", gra[i].name, gra[i].middle_grade, gra[i].final_grade);
-    }
-    
-    printf("찾으시려는 학생의 이름을 입력해 주십시오. 종료를 원하시면 0을 입력");
-    scanf("%s",search_name);
-    if(search_name == 0){
-    	exit(1);
-    }
-    else{
-    	for(int i=0;i<(sizeof(struct grade)/28)*count;i++){
-    		if(strcmp(gra[i].name,search_name)==0){
-    			printf("name : %s, middle_grade : %d, final_grade : %d\n", gra[i].name, gra[i].middle_grade, gra[i].final_grade);
-    			break;
-    		}else if(i == count-1){
-    			printf("찾으시는 학생이 존재하지 않습니다.\n");
-    		}
-    	}
-    }
-
-    free(gra);
+	int count;
+	char *nameptr = (char*)malloc(20*count);
+	printf("학생 수를 입력해주십시오 : ");
+	scanf("%d",&count);
+	struct node *nodeptr = (struct node*)malloc(sizeof(struct node)*count);
+	struct node **ret;
+	int i = 0;
+	printf("학생의 인원만큼 이름 중간 성적 기말 성적을 입력해주십시오\n");
+	while (scanf("%s%d%d", nameptr, &nodeptr->middle, &nodeptr->final)){
+		nodeptr->name = nameptr;
+		ret = (struct node **) tsearch((void *) nodeptr,(void **) &root, compare);
+		printf("\"%s\" 님이 ", (*ret)->name);
+		if (*ret == nodeptr)
+			printf("트리에 추가되었습니다.\n");
+		else{
+			printf("트리에 이미 존재합니다.\n");
+			i--;
+		}
+		nameptr += strlen(nameptr) + 1;
+		nodeptr++;
+		i++;
+		if(i==count)
+			break;
+	}
+	twalk((void *) root, print_node);
 }
